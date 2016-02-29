1ed5736490bd92209feec46a0c59e263dc667120
windgazer <martin@windgazer.nl>
2016-03-01 00:42:53 +0100

    Using async to properly run multiple requests in series. Using prism to return mocked responses (still can't find 'recordings' though)

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,12 +28,45 @@ module.exports = function(grunt) {
             tests: [ 'tmp' ]
         },
 
+        connect: {
+            server: {
+                options: {
+                    port: 8081,
+                    hostname: '127.0.0.1',
+                    middleware: function(connect, options, middlewares) {
+                        middlewares.unshift(require('grunt-connect-prism/middleware'));
+
+                        // add REST stuff
+                        middlewares.unshift(function(req, res, next) {
+                            console.log('proxy', req.url);
+
+                            res.setHeader('Access-Control-Allow-Credentials', 'true');
+                            res.setHeader('Access-Control-Allow-Headers', 'accept, x-version, content-type, authorization');
+                            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+                            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+                            res.setHeader('Access-Control-Expose-Headers', 'X-Version, X-Maintenance-Mode');
+                            res.setHeader('Access-Control-Max-Age', '1728000');
+
+                            next();
+                        });
+                        return middlewares;
+                    }
+                },
+            },
+        },
+
         prism: {
             options: {
-                mode: 'mock',
-                host: 'localhost',
+                mode: 'mockrecord',
+                host: '127.0.0.1',
+                https: false,
+                mocksPath: 'test/mocks',
                 port: 8090,
-                context: '/webdav'
+            },
+            webdav: {
+                options: {
+                    context: '/webdav'
+                }
             }
         },
 
@@ -52,7 +85,7 @@ module.exports = function(grunt) {
                     expand: true, flatten: false,
                     cwd: 'test/',
                     src: [ 'fixtures/*', 'fake/tst' ],
-                    dest: 'http://localhost:8090/webdav/'
+                    dest: 'http://localhost:8081/webdav/'
                 } ]
             },
             custom_options: {
@@ -63,7 +96,7 @@ module.exports = function(grunt) {
                     expand: true, flatten: false,
                     cwd: 'test/',
                     src: [ 'fixtures/*', 'fake/tst' ],
-                    dest: 'http://localhost:8090/webdav/'
+                    dest: 'http://localhost:8081/webdav/'
                 } ]
             }
         },
@@ -81,6 +114,7 @@ module.exports = function(grunt) {
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-connect-prism');
     grunt.loadNpmTasks('grunt-mock');
@@ -88,7 +122,7 @@ module.exports = function(grunt) {
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    grunt.registerTask('test', [ 'clean', 'prism', 'touch', 'webdavpush', 'nodeunit' ]);
+    grunt.registerTask('test', [ 'clean', 'prism:webdav', 'connect', 'touch', 'webdavpush', 'nodeunit' ]);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', [ 'jshint', 'test' ]);
--- tasks/webdavpush.js
+++ tasks/webdavpush.js
@@ -8,22 +8,45 @@
 
 'use strict';
 
-var fs = require('fs');
-var request = require('request');
+var fs = require('fs'),
+    async = require('async'),
+    request = require('request')
+;
 
 module.exports = function(grunt) {
+
+    function createHandler(done) {
+        return function resolveRequest(error, response, body) {
+            grunt.log.warn([ error, response, body ].join(', '));
+            if (!error && response.statusCode > 199 && response.statusCode < 400) {
+                done();
+            } else {
+                done(error);
+            }
+        };
+    }
+
+    function pushFile(f, done) {
+        var t = Date.now();
+        grunt.log.warn('Pushing "' + f.src + '" to ' + f.dest);
+        fs.createReadStream(f.src).pipe(request.put(f.dest, createHandler(done)));
+    }
 
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
     grunt.registerMultiTask('webdavpush', 'A one-way webdav based sync.', function() {
-        // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({
-            since: (5 * 60 * 1000) //5 minutes old
-        });
-        var filterSince = (new Date()).getTime() - ~~options.since; //5 minutes old
+
+        var done = this.async(),
+            // Merge task-specific and/or target-specific options with these defaults.
+            options = this.options({
+                since: (5 * 60 * 1000) //5 minutes old
+            }),
+            filterSince = (new Date()).getTime() - ~~options.since //5 minutes old
+        ;
 
         // Iterate over all specified file groups.
+        var filtered = [];
         this.files.forEach(function(f) {
             var src = f.src.filter(function(filepath) {
                 if (!grunt.file.exists(filepath)) {
@@ -35,10 +58,19 @@ module.exports = function(grunt) {
             }).map(function(filepath) {
                 var destination = f.dest;
                 destination = destination.replace(/^http(s?):\/(?!\/)/, 'http$1://'); //dunno why the double forward gets eaten...
-                grunt.log.warn('Pushing "' + filepath + '" to ' + destination);
-                fs.createReadStream(filepath).pipe(request.put(destination));
-                return f.dest;
-            });//.join(grunt.util.normalizelf(options.separator));
+                //pushFile(filepath, destination);
+                return {src: filepath, dest: destination};
+            });
+
+            filtered = filtered.concat(src);
+        });
+
+        grunt.log.warn(JSON.stringify(filtered));
+        async.eachSeries(filtered, pushFile, function eventually(err) {
+            if (err) {
+                grunt.fail.fatal(err);
+            }
+            done(err);
         });
     });
 
